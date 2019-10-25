6871457022b48564cb70481de0854ad095085a3e
Aleksandr Daniloff <adaniloff@wynd.eu>
2019-10-25 16:29:32 +0200

    [+] feature -> /!\ improving the RoundAreaCommand with multiple shapes !
    The goal is to calculate the area that a shape would require, as a round.
    
    However, we quickly see that the command only calculate the round area for rounds !
    We would like to use the RoundUtil to calculate the round area required by a quadrilateral shape;
    thus avoiding having calculation methods duplicated.
    
    We'll see how adapter can help us do that in the next commit!

--- src/Command/RoundAreaCommand.php
+++ src/Command/RoundAreaCommand.php
@@ -2,12 +2,18 @@
 
 namespace App\Command;
 
+use App\Library\Quadrilateral\Factory\QuadrilateralFactory;
+use App\Library\Quadrilateral\QuadrilateralUtil;
+use App\Library\Quadrilateral\ValueObject\Rectangle\Rectangle;
+use App\Library\Quadrilateral\ValueObject\Square\Square;
 use App\Library\Round\RoundUtil;
 use App\Library\Round\ValueObject\Round\Round;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class RoundAreaCommand extends Command
@@ -18,7 +24,6 @@ class RoundAreaCommand extends Command
     {
         $this
             ->setDescription('Calculate the area of a round shape.')
-            ->addArgument('diameter', InputArgument::REQUIRED, 'The diameter of the round shape.')
 
         ;
     }
@@ -26,10 +31,46 @@ class RoundAreaCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        $diameter = $input->getArgument('diameter');
 
-        $item = new Round($diameter);
+        $item = $this->askForAnItem($input, $output);
 
-        $io->success('The round area of the item is: ' . RoundUtil::area($item));
+        if ($item instanceof Round) {
+            $io->success('The round area of the item is: ' . RoundUtil::area($item));
+        } else {
+            $io->success('The round area of the item is: ' . QuadrilateralUtil::area($item));
+        }
+    }
+
+    private function askForAnItem(InputInterface $input, OutputInterface $output)
+    {
+        $questionHelper = $this->getHelper('question');
+        $question = new ChoiceQuestion(
+            'Please select the shape to enter',
+            [Round::NAME, Square::NAME, Rectangle::NAME],
+            0
+        );
+        $question->setErrorMessage('Shape %s is invalid.');
+
+        $shape = $questionHelper->ask($input, $output, $question);
+        $output->writeln('You have just selected: '.$shape);
+
+        switch ($shape) {
+            case Round::NAME:
+                $question = new Question('Please enter a diameter: ');
+                $diameter = $questionHelper->ask($input, $output, $question);
+                return new Round($diameter);
+
+            case Rectangle::NAME:
+                $question = new Question('Please enter a width: ');
+                $width = $questionHelper->ask($input, $output, $question);
+            // no-break
+
+            case Square::NAME:
+                $question = new Question('Please enter a length: ');
+                $length = $questionHelper->ask($input, $output, $question);
+                return QuadrilateralFactory::create($length, $width ?? null);
+        }
+
+        return null;
     }
 }
